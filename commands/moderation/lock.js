7b357ea3816a027d48a2341e199680dc3a26a3b2
AxelDemorest <axel.demorest@gmail.com>
2021-09-08 21:20:59 +0200

    Lock/Unlock commands

---
+++ commands/moderation/lock.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: 'lock',
+    description: 'Lock a channel',
+    category: "Moderation",
+    usage: '<channel>',
+    userPermissions: ['MANAGE_ROLES'],
+    clientPermissions: ['EMBED_LINKS', 'MANAGE_ROLES'],
+    exemples: [
+        'lock #channel',
+    ],
+    async execute(client, message, args) {
+
+        const channel = message.guild.channels.cache.get(args[0]) || message.mentions.channels.first();
+
+        if (!channel) return message.channel.send({ embeds: [client.util.errorMsg(message.author.tag, "Channel not found.")] });
+
+        await channel.permissionOverwrites.create(message.guild.roles.everyone, {
+            SEND_MESSAGES: false,
+        });
+
+        message.channel.send({ embeds: [client.util.successMsg(message.author.tag, `${channel} has been locked.`)] });
+
+    },
+};
---
+++ commands/moderation/unlock.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: 'unlock',
+    description: 'Unlock a channel',
+    category: "Moderation",
+    usage: '<channel>',
+    userPermissions: ['MANAGE_ROLES'],
+    clientPermissions: ['EMBED_LINKS', 'MANAGE_ROLES'],
+    exemples: [
+        'unlock #channel',
+    ],
+    async execute(client, message, args) {
+
+        const channel = message.guild.channels.cache.get(args[0]) || message.mentions.channels.first();
+
+        if (!channel) return message.channel.send({ embeds: [client.util.errorMsg(message.author.tag, "Channel not found.")] });
+
+        await channel.permissionOverwrites.create(message.guild.roles.everyone, {
+            SEND_MESSAGES: true,
+        });
+
+        message.channel.send({ embeds: [client.util.successMsg(message.author.tag, `${channel} has been unlocked.`)] });
+
+    },
+};
